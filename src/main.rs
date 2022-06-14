72332cd347d488dabc53d4969c846cb5a030b9c3
mr-wang1009 <1115378579@qq.com>
2022-06-14 11:34:14 +0800

    feat: setup app

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+// 使用cargo创建项目 
+// cargo new 项目名
+// ==========================
+// Cargo.toml 配置文件
+// edition rust 版本
+// dependencies 项目依赖包
+// ==========================
+// Cargo build --release // flag 优化
+// 开发 发布
+// Cargo run
+// Cargo check
+fn main() {
+    println!("Hello, world!");
+}
